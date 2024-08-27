9ab6a46493e0a9ffa5937bc6119b5dc6fb16e0a4
Hiba Kharma <hibakharma@gmail.com>
2024-08-27 13:47:33 +0300

    put the script in footer :)

--- resources/views/Pages/My_Classes/My_Classes.blade.php
+++ resources/views/Pages/My_Classes/My_Classes.blade.php
@@ -285,23 +285,7 @@
     </div>
 
     <!-- row closed -->
-    <script>
-        function check_all(className,elem) {
-            var elements=document.getElementsByClassName(className);
-            var l=elements.length;
-            if(elem.checked){
-                for(var i=0;i<l;i++){
-                    elements[i].checked=true;
-                }
-            }
-            else {
-                for(var i=0;i<l;i++){
-                    elements[i].checked=false;
-                }
-            }
 
-        }
-    </script>
 @endsection
 @section('js')
     @toastr_js
